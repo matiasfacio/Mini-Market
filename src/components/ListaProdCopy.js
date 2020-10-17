f9cfbf5db5c9c539ffd6d8dfb636252e45b1da61
Matias Facio <matiaspersonal@gmail.com>
2020-10-17 15:39:01 +0200

    some prettier

--- src/components/ListaProdCopy.js
+++ src/components/ListaProdCopy.js
@@ -1,74 +1,113 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { GlobalContext } from '../context/GlobalContext'
-import '../App.css'
-import DisplayCart from './DisplayCart'
-import ProductInformation from './ProductInformation'
-import Form from './Form'
-
+import React, { useContext, useEffect, useState } from "react";
+import { GlobalContext } from "../context/GlobalContext";
+import "../App.css";
+import DisplayCart from "./DisplayCart";
+import ProductInformation from "./ProductInformation";
+import Form from "./Form";
 
 function ListaProd() {
-    const { ProductsAvailable, Cart, CountCart, dispatch} = useContext(GlobalContext)
-    
-    const [newItem, setNewItem] = useState({name: '', id: ProductsAvailable.length+2, stock: 0, description: '', price: 0, available: false})
+  const { ProductsAvailable, Cart, CountCart, dispatch } = useContext(
+    GlobalContext
+  );
 
-    const [menuClick,setmenuClick] = useState(false)
+  const [newItem, setNewItem] = useState({
+    name: "",
+    id: ProductsAvailable.length + 2,
+    stock: 0,
+    description: "",
+    price: 0,
+    available: false,
+  });
 
+  const [menuClick, setmenuClick] = useState(false);
 
+  useEffect(() => {
+    return;
+  }, [menuClick]);
 
-    useEffect(()=> {
-        return
-    }, [menuClick])
-    
-
-    return (
-        <div>
-            <div className ="title"><h1>Mini Market</h1></div>
-            <nav>
-                <ul>
-                        <li onClick = { () => setmenuClick(!menuClick)}>{!menuClick ? 'Show Cart':'Hide Cart'}</li>
-                            <p style = {{color:'red'}}>({Cart.length})</p>
-                </ul>
-            </nav>
-            <div className = "_listsDisplay">  
-                <div className = "_listaProducts">
-                    <div className = "title_list">
-                        <h2>Products</h2>
-                        <p>*our products with love*</p>
-                    </div>
-                    <div className = "producto container_list" style = {{marginBottom: 20}}>
-                        <div className ="producto_id" style = {{fontSize: '1.2rem'}}>ID</div>
-                        <div className ="producto_name" style = {{fontSize: '1.2rem'}}>NAME</div>
-                        <div className ="producto_price" style = {{fontSize: '1.2rem'}}>PRICE</div>
-                    </div>
-                    <div className = "container_list">
-                        {ProductsAvailable.map((t, index) => {
-                        return <div key={index} className = 'producto'>
-                                    <div className ="producto_id">{t.id})</div> 
-                                    <div className ="producto_name">{t.name}</div>
-                                    <div className = "producto_price">€ {(t.price.toFixed(2))}</div>
-                                    <button className = "producto_button_available" style = {{
-                                        backgroundColor: !t.available ? 'red': 'green'}} 
-                                        onClick = {()=> dispatch({type: 'AVAILABLE', index})}>
-                                        {t.available ? 'Deactivate': 'Activate'}
-                                    </button>
-
-                                    {t.available ? <button  className = "producto_button_add" onClick = {()=> dispatch({type: 'ADD_CART', index})}>ADD</button> : ''}
-                                    <button className = "producto_display_info" style = {{border: t.display ? '2px red solid': 'none'}}onMouseOver = {() => {dispatch({type: 'CLEAR_INFO'}); dispatch({type: 'INFO_ITEM', index})}}>INFO</button>
-                                </div>
-                        })}
-                    </div>
-                    <div className ="secondPage"> 
-                        <Form />
-                        <ProductInformation />
-                    </div>
-                </div>
-                
-                <div className = "container_right">
-                    {menuClick ? <DisplayCart />: ''}                   
-                </div>
+  return (
+    <div>
+      <div className="title">
+        <h1>Mini Market</h1>
+      </div>
+      <nav>
+        <ul>
+          <li onClick={() => setmenuClick(!menuClick)}>
+            {!menuClick ? "Show Cart" : "Hide Cart"}
+          </li>
+          <p style={{ color: "red" }}>({Cart.length})</p>
+        </ul>
+      </nav>
+      <div className="_listsDisplay">
+        <div className="_listaProducts">
+          <div className="title_list">
+            <h2>Products</h2>
+            <p>*our I products with love*</p>
+          </div>
+          <div className="producto container_list" style={{ marginBottom: 20 }}>
+            <div className="producto_id" style={{ fontSize: "1.2rem" }}>
+              ID
             </div>
+            <div className="producto_name" style={{ fontSize: "1.2rem" }}>
+              NAME
+            </div>
+            <div className="producto_price" style={{ fontSize: "1.2rem" }}>
+              PRICE
+            </div>
+          </div>
+          <div className="container_list">
+            {ProductsAvailable.map((t, index) => {
+              return (
+                <div key={index} className="producto">
+                  <div className="producto_id">{t.id})</div>
+                  <div className="producto_name">{t.name}</div>
+                  <div className="producto_price">€ {t.price.toFixed(2)}</div>
+                  <button
+                    className="producto_button_available"
+                    style={{
+                      backgroundColor: !t.available ? "red" : "green",
+                    }}
+                    onClick={() => dispatch({ type: "AVAILABLE", index })}
+                  >
+                    {t.available ? "Deactivate" : "Activate"}
+                  </button>
+
+                  {t.available ? (
+                    <button
+                      className="producto_button_add"
+                      onClick={() => dispatch({ type: "ADD_CART", index })}
+                    >
+                      ADD
+                    </button>
+                  ) : (
+                    ""
+                  )}
+                  <button
+                    className="producto_display_info"
+                    style={{ border: t.display ? "2px red solid" : "none" }}
+                    onMouseOver={() => {
+                      dispatch({ type: "CLEAR_INFO" });
+                      dispatch({ type: "INFO_ITEM", index });
+                    }}
+                  >
+                    INFO
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+          <div className="secondPage">
+            <Form />
+            <ProductInformation />
+          </div>
         </div>
-    )
+
+        <div className="container_right">
+          {menuClick ? <DisplayCart /> : ""}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ListaProd
+export default ListaProd;
--- src/context/GlobalContext.js
+++ src/context/GlobalContext.js
@@ -1,103 +1,119 @@
-import React, { createContext, useReducer } from 'react';
-import Products from '../data/Products'
+import React, { createContext, useReducer } from "react";
+import Products from "../data/Products";
 
 export const GlobalContext = createContext();
 
-
 const reducerProducts = (state, action) => {
-   
-    switch (action.type) {
-        case 'ADD':
-            return {
-                    ...state, 
-                    ProductsAvailable: [...state.ProductsAvailable, action.newItem]
-            }
+  switch (action.type) {
+    case "ADD":
+      return {
+        ...state,
+        ProductsAvailable: [...state.ProductsAvailable, action.newItem],
+      };
 
-        case 'AVAILABLE':
-            return { 
-                    ...state, 
-                    ProductsAvailable: state.ProductsAvailable.map((t, index) => {
-                    return index === action.index ? {...t, available: !t.available}: t
-            })}
+    case "AVAILABLE":
+      return {
+        ...state,
+        ProductsAvailable: state.ProductsAvailable.map((t, index) => {
+          return index === action.index ? { ...t, available: !t.available } : t;
+        }),
+      };
 
-        case 'ADD_CART':
+    case "ADD_CART":
+      return {
+        ...state,
+        Cart: [
+          ...state.Cart,
+          { ...state.ProductsAvailable[action.index], quantity: 1 },
+        ],
+        CountCart:
+          state.CountCart + state.ProductsAvailable[action.index].price,
+      };
 
-            return {
-                ...state,
-                Cart: [...state.Cart, {...state.ProductsAvailable[action.index], quantity: 1 }],
-                CountCart: state.CountCart + state.ProductsAvailable[action.index].price
-            }
-        
-        case 'UPDATE_CART_INC': 
-            return {
-                ...state,
-                Cart: state.Cart.map((t, index) => {
-                    return index === action.index ? {...t, quantity : t.quantity+1} : t
-                }),
-                CountCart: state.CountCart + state.Cart[action.index].price
-            }
-        
-        case 'UPDATE_CART_DEC': 
-            return {
-                ...state,
-                Cart: state.Cart.map((t, index) => {
-                    return index === action.index ? {...t, quantity : t.quantity-1} : t
-                }),
-                CountCart: state.CountCart - state.Cart[action.index].price
-            }
-                  
-        
-        case 'DEL_CART':               
-            return {
-                    ...state, 
-                    CountCart: state.CountCart - state.Cart[action.index].price * state.Cart[action.index].quantity,
-                    // CountCart: state.CountCart - state.ProductsAvailable[state.ProductsAvailable.indexOf(action.t)].price,
-                    Cart: state.Cart.filter((t,index) => { return index !== action.index})
-                }
-        
-        case 'EMPTY_CART':
-            return {
-                    ...state,
-                    Cart: [],
-                    CountCart: 0
-            }
+    case "UPDATE_CART_INC":
+      return {
+        ...state,
+        Cart: state.Cart.map((t, index) => {
+          return index === action.index
+            ? { ...t, quantity: t.quantity + 1 }
+            : t;
+        }),
+        CountCart: state.CountCart + state.Cart[action.index].price,
+      };
 
+    case "UPDATE_CART_DEC":
+      return {
+        ...state,
+        Cart: state.Cart.map((t, index) => {
+          return index === action.index
+            ? { ...t, quantity: t.quantity - 1 }
+            : t;
+        }),
+        CountCart: state.CountCart - state.Cart[action.index].price,
+      };
 
-        case 'INFO_ITEM':
-            return {
-                    ...state,
-                    ProductsAvailable: state.ProductsAvailable.map((t,index) => {
-                        return index === action.index ? {...t, display: !t.display} : t
-                    })
-            }
-        
-        case 'CLEAR_INFO':
-            return {
-                    ...state,
-                    ProductsAvailable: state.ProductsAvailable.map(t => {
-                        return {...t, display: false}
-                    })
-            }
+    case "DEL_CART":
+      return {
+        ...state,
+        CountCart:
+          state.CountCart -
+          state.Cart[action.index].price * state.Cart[action.index].quantity,
+        // CountCart: state.CountCart - state.ProductsAvailable[state.ProductsAvailable.indexOf(action.t)].price,
+        Cart: state.Cart.filter((t, index) => {
+          return index !== action.index;
+        }),
+      };
 
-        default: return state
-    }
-}
+    case "EMPTY_CART":
+      return {
+        ...state,
+        Cart: [],
+        CountCart: 0,
+      };
+
+    case "INFO_ITEM":
+      return {
+        ...state,
+        ProductsAvailable: state.ProductsAvailable.map((t, index) => {
+          return index === action.index ? { ...t, display: !t.display } : t;
+        }),
+      };
+
+    case "CLEAR_INFO":
+      return {
+        ...state,
+        ProductsAvailable: state.ProductsAvailable.map((t) => {
+          return { ...t, display: false };
+        }),
+      };
+
+    default:
+      return state;
+  }
+};
 
 const GlobalContextFunction = (props) => {
+  const [{ ProductsAvailable, Cart, CountCart }, dispatch] = useReducer(
+    reducerProducts,
+    {
+      ProductsAvailable: Products.map((t) => {
+        return { ...t, available: false, display: false };
+      }),
+      Cart: [],
+      CountCart: 0,
+    }
+  );
 
-    const [{ProductsAvailable, Cart, CountCart} , dispatch] = useReducer(reducerProducts, {ProductsAvailable:Products.map(t => {
-        return {...t, available: false, display: false}
-    }), Cart: [], CountCart: 0})
+  console.log(ProductsAvailable);
+  console.log(Cart);
 
-    console.log(ProductsAvailable)
-    console.log(Cart)
+  return (
+    <GlobalContext.Provider
+      value={{ ProductsAvailable, Cart, CountCart, dispatch }}
+    >
+      {props.children}
+    </GlobalContext.Provider>
+  );
+};
 
-    return (
-        <GlobalContext.Provider value = {{ProductsAvailable, Cart, CountCart, dispatch}}>
-            {props.children}
-        </GlobalContext.Provider>
-    )
-}
-
-export default GlobalContextFunction
-
+export default GlobalContextFunction;
